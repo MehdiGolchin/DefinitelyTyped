086e391a9e137c74ee99ed9dc0ce3e747798c187
Mehdi Golchin <m.kharazi@evision-software.com>
2019-01-04 09:26:12 +0100

    fix tslint problems

--- types/setup-polly-jest/index.d.ts
+++ types/setup-polly-jest/index.d.ts
@@ -1,18 +1,18 @@
-// Type definitions for setup-polly-jest 0.5.2
+// Type definitions for setup-polly-jest v0.5.2
 // Project: https://github.com/gribnoysup/setup-polly-jest
 // Definitions by: Mehdi Golchin <https://github.com/MehdiGolchin>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
-import { PollyConfig, Polly } from '@pollyjs/core'
+import { PollyConfig, Polly } from '@pollyjs/core';
 
 /**
  * Shared context to keep polly instance and
  * options for a specific run
  */
 export interface PollyContext {
-    polly: Polly,
-    options: PollyConfig
+    readonly polly: Polly;
+    readonly options: PollyConfig;
 }
 
 /**
@@ -20,8 +20,8 @@ export interface PollyContext {
  * add beforeAll/afterAll hooks that will activate/deactivate
  * Polly when running test suite
  *
- * @param {Object} defaults Polly default options
+ * @param {PollyConfig} defaults Polly default options
  *
- * @returns {Object} Context with `polly` property
+ * @returns {PollyContext} Context with `polly` property
  */
 export function setupPolly(config: PollyConfig): PollyContext;
